42b4e005ac06c86f4e0bcf5bd5ee71f5c1a1749a
OddC <oddc2000@outlook.com>
2024-12-03 00:17:02 +1100

    Remove unused files

--- ignition/modules/TodoList.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
-
-const TodoListModule = buildModule('TodoListModule', (m) => {
-  const todoList = m.contract('TodoList')
-
-  return { todoList }
-})
-
-export default TodoListModule

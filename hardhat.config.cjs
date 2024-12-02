7072fda5188bc819e87630f7279f9a1d89a5f180
OddC <oddc2000@outlook.com>
2024-12-02 17:20:00 +1100

    fix: fix deploying in Node.js 22

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,11 +1,15 @@
-import { vars, type HardhatUserConfig } from 'hardhat/config.js'
-import '@nomicfoundation/hardhat-toolbox-viem'
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { vars } = require('hardhat/config')
+require('@nomicfoundation/hardhat-toolbox-viem')
 
 const SEPOLIA_PRIVATE_KEY = vars.get('SEPOLIA_PRIVATE_KEY')
 const INFURA_API_KEY = vars.get('INFURA_API_KEY')
 const ETHERSCAN_API_KEY = vars.get('ETHERSCAN_API_KEY')
 
-const config: HardhatUserConfig = {
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
   solidity: '0.8.27',
   networks: {
     sepolia: {
@@ -19,5 +23,3 @@ const config: HardhatUserConfig = {
     }
   }
 }
-
-export default config

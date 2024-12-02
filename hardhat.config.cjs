2b14245c4bb7740816a43f6156caa0a36ba67f20
OddC <oddc2000@outlook.com>
2024-12-02 17:44:03 +1100

    fix build in Vercel

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -2,24 +2,30 @@
 const { vars } = require('hardhat/config')
 require('@nomicfoundation/hardhat-toolbox-viem')
 
-const SEPOLIA_PRIVATE_KEY = vars.get('SEPOLIA_PRIVATE_KEY')
-const INFURA_API_KEY = process.env.INFURA_API_KEY ?? vars.get('INFURA_API_KEY')
+const SEPOLIA_PRIVATE_KEY = vars.get('SEPOLIA_PRIVATE_KEY', '')
+const INFURA_API_KEY = vars.get('INFURA_API_KEY', '')
 const ETHERSCAN_API_KEY = vars.get('ETHERSCAN_API_KEY', '')
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
+const config = {
   solidity: '0.8.27',
-  networks: {
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: vars.has('SEPOLIA_PRIVATE_KEY') ? [SEPOLIA_PRIVATE_KEY] : []
-    }
-  },
-  etherscan: {
-    apiKey: {
-      sepolia: ETHERSCAN_API_KEY
-    }
-  }
+  ...(process.env.NODE_ENV === 'development'
+    ? {
+        networks: {
+          sepolia: {
+            url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+            accounts: [SEPOLIA_PRIVATE_KEY]
+          }
+        },
+        etherscan: {
+          apiKey: {
+            sepolia: ETHERSCAN_API_KEY
+          }
+        }
+      }
+    : {})
 }
+
+module.exports = config
